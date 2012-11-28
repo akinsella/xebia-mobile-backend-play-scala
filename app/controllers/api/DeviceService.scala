a0be7731515addd99e1927410ee6c6bf68c8d12e
Xavier Bucchiotty <xbucchiotty@xebia.fr>
2012-11-28 18:41:41 +0100

    First version of Swagger integration

--- app/controllers/api/DeviceService.scala
+++ app/controllers/api/DeviceService.scala
@@ -2,50 +2,92 @@ package controllers.api
 
 import models._
 import play.api.mvc.{Action, Controller}
-import play.api.libs.json._
+import play.api.libs.json.Json.toJson
+import play.api.libs.json.Json
+import com.wordnik.swagger.annotations._
 
-object DeviceService extends Controller  {
+@Api(value = "/api/device", description = "Operations about devices", listingPath = "/api-docs.{format}/device")
+object DeviceService extends Controller {
 
-  def devices = Action { implicit request =>
+  @ApiOperation(value = "Show all devices", httpMethod = "GET")
+  def devices = Action {
+    implicit request =>
     // Necessary if you want to run a mobile app in local browser
-    if (request.method == "OPTIONS") {
-      println("OPTIONS")
-      Ok.withHeaders(
-        "Access-Control-Allow-Origin" -> "*",
-        "Access-Control-Allow-Methods" -> "GET,POST",
-        "Access-Control-Max-Age" -> "360",
-        "Access-Control-Allow-Headers" -> "x-requested-with"
-      )
-    } else {
-      val json = Json.toJson(Device.all)
-      Ok(json).as("application/json").withHeaders(
-        "Access-Control-Allow-Origin" -> "*",
-        "Access-Control-Allow-Methods" -> "GET,POST",
-        "Access-Control-Max-Age" -> "360",
-        "Access-Control-Allow-Headers" -> "x-requested-with"
-      )
+      if (request.method == "OPTIONS") {
+        println("OPTIONS")
+        Ok.withHeaders(
+          "Access-Control-Allow-Origin" -> "*",
+          "Access-Control-Allow-Methods" -> "GET,POST,PUT",
+          "Access-Control-Max-Age" -> "1",
+          "Access-Control-Allow-Headers" -> "x-requested-with,content-type"
+        )
+      } else {
+        val json = toJson(Device.all)
+        Ok(json).as("application/json").withHeaders(
+          "Access-Control-Allow-Origin" -> "*",
+          "Access-Control-Allow-Methods" -> "GET,POST,PUT",
+          "Access-Control-Max-Age" -> "360",
+          "Access-Control-Allow-Headers" -> "x-requested-with"
+        )
+      }
+  }
+
+  @ApiOperation(value = "Find one device by id", httpMethod = "GET")
+  def show(@ApiParam(name = "id", value = "Internal id of the device", required = true) id: Long) = Action {
+    Ok(toJson(Device.findById(id))).as("application/json")
+  }
+
+  @ApiOperation(value = "Create a new device", httpMethod = "PUT")
+  @ApiParamsImplicit(Array(
+    new ApiParamImplicit(value = "Add a new device in the database", required = true, dataType = "Device", paramType = "body")
+  ))
+  def create() = Action {
+    implicit request => {
+      request.body.asJson match {
+        case None => NotAcceptable
+        case Some(query) => {
+          Device.create(query.as[Device])
+          Created
+        }
+      }
     }
   }
 
-  def show(id: Long) = Action {
-    Ok(Json.toJson(Device.findById(Some(id)))).as("application/json")
+  @ApiOperation(value = "Update an existing device", httpMethod = "POST")
+  @ApiErrors(Array(
+    new ApiError(code = 404, reason = "Device not found")))
+  def save(@ApiParam(name = "id", value = "Internal id of the device", required = true) id: Long) = Action {
+    implicit request => request.body.asText match {
+      case None => NotAcceptable
+      case Some(query) => {
+        if (Device.findById(id).isDefined) {
+          val device = Json.parse(query).as[Device]
+          Device.update(device)
+          println(device)
+          Ok
+        }
+        else {
+          NotFound
+        }
+      }
+    }
   }
 
-  def create() = Action { implicit request =>
-    val device = Json.parse(request.body.asText.get).as[Device]
-    println(device)
-    Created
-  }
-
-  def save(id: Option[Long]) = Action { implicit request =>
-    val device = Json.parse(request.body.asText.get).as[Device]
-    println(device)
-    Ok
-  }
-
-  def delete(id: Long) = Action {
-    println("deleting {id}", id)
-    Device.delete(id)
-    Ok
+  @ApiOperation(value = "Delete an existing device", httpMethod = "DELETE")
+  @ApiErrors(Array(
+    new ApiError(code = 404, reason = "Device not found")))
+  def delete(@ApiParam(name = "id", value = "Internal id of the device", required = true) id: Long) = Action {
+    implicit request => request.body.asText match {
+      case None => NotAcceptable
+      case Some(query) => {
+        if (Device.findById(id).isDefined) {
+          Device.delete(id)
+          Ok
+        }
+        else {
+          NotFound
+        }
+      }
+    }
   }
 }
--- project/Build.scala
+++ project/Build.scala
@@ -7,14 +7,16 @@ object ApplicationBuild extends Build {
     val appName         = "xebia-mobile-backend-play-scala"
     val appVersion      = "1.0-SNAPSHOT"
 
-    val appDependencies = Seq(
+  val appDependencies = Seq(
       // Add your project dependencies here,
       "com.notnoop.apns" % "apns" % "0.1.6",
       "com.twitter" % "querulous" % "2.6.5",
       "mysql" % "mysql-connector-java" % "5.1.21",
       "postgresql" % "postgresql" % "9.1-901-1.jdbc4",
       "org.fusesource.scalate" % "scalate-core" % "1.5.3",
-      "net.debasishg" % "redisclient_2.9.2" % "2.7"
+      "net.debasishg" % "redisclient_2.9.2" % "2.7",
+      "com.wordnik" %% "swagger-play2" % "1.2.0",
+      "com.wordnik" %% "swagger-play2-utils" % "1.2.0"
       /*,"eu.teamon" %% "play-navigator" % "0.4.0"*/
     )
 
