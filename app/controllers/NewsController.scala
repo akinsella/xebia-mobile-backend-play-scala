80c5f490f16a51f71f18c7e1b852806af52bfc3d
Alexis Kinsella <alexis.kinsella@gmail.com>
2013-01-16 23:23:06 +0100

    WIP - Persistence for News entity, and fix of views and controller

--- app/controllers/NewsController.scala
+++ app/controllers/NewsController.scala
@@ -34,39 +34,48 @@ object NewsController extends Controller with SecureSocial {
       "content" -> nonEmptyText,
       "imageUrl" -> nonEmptyText
     )
-    {
-      (title, content, imageUrl) => News.apply(title, content, imageUrl)
-    }
-    {
-      news =>  Some(news.title, news.content, news.imageUrl)
-    }
+    { (title, content, imageUrl) => News.apply(title, content, imageUrl) }
+    { news =>  Some(news.title, news.content, news.imageUrl) }
   )
 
   /**
    * Display an empty form.
    */
-  def create = SecuredAction { implicit request =>
-    Ok(html.News.form(request.user, None, newsForm))
+  def createForm = SecuredAction { implicit request =>
+    Ok(html.News.create(request.user, None, newsForm))
   }
 
   /**
    * Display a form pre-filled with an existing News.
    */
-  def edit(newsId: Long) = SecuredAction { implicit request =>
-    News.findById(newsId).map { news =>
-      Ok(html.News.form(request.user, Some(newsId), newsForm.fill(news)))
+  def updateForm(id: Long) = SecuredAction { implicit request =>
+    News.findById(id).map { news =>
+      Ok(html.News.update(request.user, Some(id), newsForm.fill(news)))
     }.getOrElse( Redirect(routes.NewsController.index()) )
   }
 
   /**
    * Handle form submission.
    */
-  def submit = SecuredAction { implicit request =>
+  def create = SecuredAction { implicit request =>
     newsForm.bindFromRequest.fold(
-      errors => BadRequest(html.News.form(request.user, None, errors)),
+      errors => BadRequest(html.News.create(request.user, None, errors)),
       news => {
         News.create(news)
-        Redirect(routes.NewsController.index())
+        Redirect( routes.NewsController.index() )
+      }
+    )
+  }
+
+  /**
+   * Handle form submission.
+   */
+  def update(id: Long) = SecuredAction { implicit request =>
+    newsForm.bindFromRequest.fold(
+      errors => BadRequest(html.News.update(request.user, None, errors)),
+      news => {
+        News.update(id, news)
+        Redirect( routes.NewsController.index() )
       }
     )
   }
@@ -74,9 +83,9 @@ object NewsController extends Controller with SecureSocial {
   /**
    * Delete a task
    */
-  def delete(newsId: Long) = SecuredAction { implicit request =>
-    News.delete(newsId)
-    Redirect(routes.NewsController.index())
+  def delete(id: Long) = SecuredAction { implicit request =>
+    News.delete(id)
+    Redirect( routes.NewsController.index() )
   }
 
 }
